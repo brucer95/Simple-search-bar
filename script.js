0da49177c882301ca479c9c5fc33a321b2508a04
brucer95 <scuteri26@gmail.com>
2023-09-01 03:28:31 -0300

    Simple search bar with HTML,CSS and JS

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("keyup", (e) => {
+	if (e.target.matches("#buscador")) {
+		if (e.key === "Escape") e.target.value = "";
+
+		const searchText = e.target.value.toLowerCase();
+		const articulos = document.querySelectorAll(".articulo");
+		let resultadosEncontrados = false;
+
+		articulos.forEach((articulo) => {
+			const contenido = articulo.textContent.toLowerCase();
+			if (contenido.includes(searchText)) {
+				articulo.style.display = "block";
+				resultadosEncontrados = true;
+			} else {
+				articulo.style.display = "none";
+			}
+		});
+
+		const mensajeNoEncontrado = document.getElementById("search-not-found");
+		if (!resultadosEncontrados) {
+			mensajeNoEncontrado.style.display = "block";
+		} else {
+			mensajeNoEncontrado.style.display = "none";
+		}
+	}
+});
